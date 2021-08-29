683abf5aa25668fb6d91eab3207e3c0acd1757d1
umutsolmaz <umutsolmaz00@gmail.com>
2021-08-29 21:11:09 +0300

    initial commit

---
+++ observer-pattern/src/main/java/observer/pattern/ObserverPatternApplication.java
@@ -0,0 +1,21 @@
+package observer.pattern;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.microservices.ecommerce")
+public class ObserverPatternApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObserverPatternApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
